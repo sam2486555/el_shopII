2923d776f59de3e2b70eaa59ab8c815ca7df9a08
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-10-27 23:41:26 +0300

    add store views.py

---
+++ store/views.py
@@ -0,0 +1,56 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
+from store.models import Link, Product
+from store.permissions import IsActive
+from store.serializers import LinkSerializer, ProductSerializer
+
+
+class LinkCreateAPIView(CreateAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [IsActive]
+
+
+class LinkListAPIView(ListAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [IsActive]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('country',)
+
+
+class LinkRetrieveAPIView(RetrieveAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [IsActive]
+
+
+class LinkUpdateAPIView(UpdateAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [IsActive]
+
+    def perform_update(self, serializer):
+        if "debt" in serializer.validated_data:
+            serializer.validated_data.pop("debt")
+            raise Exception("Вы не можете менять поле задолженности")
+        super().perform_update(serializer)
+
+
+class LinkDestroyAPIView(DestroyAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [IsActive]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActive]
